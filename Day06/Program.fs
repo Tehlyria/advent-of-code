9786a2e182548d60c6edfedc9e0a48df7a3ce01d
Daniel Below <dbelow@hotmail.de>
2018-12-06 08:51:23 +0100

    + day06 - part two

--- Day06/Program.fs
+++ Day06/Program.fs
@@ -7,7 +7,7 @@ let manhattanDist (p1: Point) (p2: Point) =
     
 let parseCoordLine l ln =
     match l with 
-    | Utility.Regex @"([0-9]+),\s([0-9]+)" [xS ; yS] ->
+    | Utility.Regex @"([0-9]+),\s([0-9]+)" [xS ; yS] -> 
         { x = int xS ; y = int yS ; v = Some ln }
     | _ -> failwith "Could not parse line."       
 
@@ -33,40 +33,60 @@ let countArea width height (anchors: List<Point>) (sq: seq<Point>) =
     |> Seq.iter (fun elem ->
         match elem with 
         | { x = ex ; y = ey ; v = ev } ->
-            // remove elements which have are closest to a cell at the edges
             if ex = 0 || ey = 0 || ex = width || ey = height then 
                 match ev with 
                 | Some v -> 
-                    orig <- List.filter (fun x -> x.v <> Some v) orig
+                    orig <- orig |> List.filter (fun x -> x.v <> Some v)
                 | _ -> ()                    
     )
     
     sq 
-    |> Seq.filter (fun (e: Point) -> List.exists (fun (x: Point) -> x.v = e.v) orig)
-    |> Seq.map (fun (e: Point) -> e.v)
+    |> Seq.filter (fun e -> 
+        orig |> List.exists (fun x -> x.v = e.v)
+    )
+    |> Seq.map (fun e -> e.v)
     |> Seq.groupBy (id)
     |> Seq.map (snd >> Seq.length)
     |> Seq.max
-
+    
+let getSize inp =
+    let w = (inp |> List.map (fun elem -> elem.x) |> List.max) + 1 
+    let h = (inp |> List.map (fun elem -> elem.y) |> List.max) + 1 
+    (w, h)
+    
 let partOne inp =
     let l = inp |> toCoordList
-    let width = (l |> List.map (fun elem -> elem.x) |> List.max) + 1
-    let height = (l |> List.map (fun elem -> elem.y) |> List.max) + 1
+    let (width, height) = getSize l
     
     generateCoords width height
     |> Seq.map (fun elem -> 
-        let { x = px; y = py; v = pv } = elem
         let closestPt = findClosestPoint elem l
-        
         if (Seq.isEmpty closestPt) then failwith "Should never be empty!"
         
         if (Seq.length closestPt > 1) then 
-            { x = px ; y = py ; v = None  }
+            { x = elem.x ; y = elem.y ; v = None  }
         else 
             let hd = Seq.head closestPt
-            { x = px ; y = py ; v = hd.v }
+            { x = elem.x ; y = elem.y ; v = hd.v }
     )
-    |> (countArea width height l)
+    |> countArea width height l
+    
+let partTwo inp =
+    let l = inp |> toCoordList
+    let (width, height) = getSize l
+    
+    // remove elements which have are closest to a cell at the edges
+    generateCoords width height
+    |> Seq.map (fun elem -> 
+        let totalDist = l |> List.map (manhattanDist elem) |> List.sum
+        { x = elem.x ; y = elem.y ; v = Some totalDist }
+    )
+    |> Seq.filter (fun elem -> 
+        match elem with 
+        | { x = _ ; y = _ ; v = Some ev } -> ev < 10000
+        | _ -> failwith "Error"            
+    )
+    |> Seq.length
  
 [<EntryPoint>]
 let main argv =
@@ -76,5 +96,6 @@ let main argv =
     let path = "input.txt"
 #endif
     let inp = Utility.readFile path
-    inp |> partOne |> printf "\nPart One: %d\n"
+    inp |> partOne |> printf "Part One: %d\n"
+    inp |> partTwo |> printf "Part Two: %d\n"
     0
